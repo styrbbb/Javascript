9618f0e4286cfd2b21d8bd8701e6c71a21b008ae
thorian <115464923+styrbbb@users.noreply.github.com>
2024-10-31 10:58:43 +0200

    ül2

---
+++ ylesanne02/ylesanne02.js
@@ -0,0 +1,10 @@
+//Thorian Perk
+//31.10.2024
+//Javascript ülesanne 2
+
+
+let mark1 = 2; 
+let mark2 = 38; 
+let mark3 = 59;
+
+let kellaaeg = `Oodatav tulemus näiteks: ${mark1}:${mark2}:${mark3}PM`;
