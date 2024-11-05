ed83bcd5dcbee23497e73f4ead823e6143769294
thorian <115464923+styrbbb@users.noreply.github.com>
2024-11-05 11:31:09 +0200

    ül4

--- ylesanne04/ylesanne04.js
+++ ylesanne04/ylesanne04.js
@@ -2,3 +2,14 @@
 //05.11.2024
 //Javascript ülesanne 4
 
+//Nädalapäevad
+let nadalapaevad = ["Esmaspäev","Teisipäev","Kolmapäev","Neljapäev","Reede"];
+console.log("Massiiv: " + nadalapaevad.length);
+nadalapaevad.push("Laupäev","Pühapäev");
+console.log("Lisatud Laupäev ja Pühapäev: " + nadalapaevad.length);
+console.log("Sorteeri kahanevalt: " + nadalapaevad.reverse());
+console.log("Kuva viimane: " + nadalapaevad.slice(0,1));
+
+
+
+
