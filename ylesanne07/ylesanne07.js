1904d5a93c57eb824ee023c9c981ae0e131ac046
thorian <115464923+styrbbb@users.noreply.github.com>
2024-11-29 13:51:53 +0200

    yl7

--- ylesanne07/ylesanne07.js
+++ ylesanne07/ylesanne07.js
@@ -2,3 +2,15 @@
 //29.11.2024
 //Javascript ülesanne 7
 
+//Tooted
+
+const tooted = ["Õunad", "Piim", "Leib", "Juust", "Tomatid", "Kanafilee", "Muna", "Sibul", "Apelsinid", "Riis", "Jogurt", "Kartul", "Kalafilee", "Pasta", "Jogurtijook", "Porgandid", "Virsikud", "Pähklid", "Rosinad", "Kapsas", "Kreeka jogurt", "Veiseliha", "Banaanid", "Oliivid", "Mandlid", "Magus kartul", "Greibid"];
+
+for (let i = 0; i < tooted.length; i++) {
+  if (i === 10) {
+    break;
+  }
+  if (tooted[i] != "Muna" && tooted[i] != "Sibul" && tooted[i] != "Riis"  ) {
+    console.log((i+1)+". "+tooted[i]);
+  }
+}
