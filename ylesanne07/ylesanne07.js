664acd06029749e23f3386cf9ff004d6f5b540d8
thorian <115464923+styrbbb@users.noreply.github.com>
2024-12-02 15:34:08 +0200

    yl7

--- ylesanne07/ylesanne07.js
+++ ylesanne07/ylesanne07.js
@@ -3,7 +3,6 @@
 //Javascript ülesanne 7
 
 //Tooted
-
 const tooted = ["Õunad", "Piim", "Leib", "Juust", "Tomatid", "Kanafilee", "Muna", "Sibul", "Apelsinid", "Riis", "Jogurt", "Kartul", "Kalafilee", "Pasta", "Jogurtijook", "Porgandid", "Virsikud", "Pähklid", "Rosinad", "Kapsas", "Kreeka jogurt", "Veiseliha", "Banaanid", "Oliivid", "Mandlid", "Magus kartul", "Greibid"];
 
 for (let i = 0; i < tooted.length; i++) {
@@ -14,3 +13,41 @@ for (let i = 0; i < tooted.length; i++) {
     console.log((i+1)+". "+tooted[i]);
   }
 }
+
+//Temperatuurid
+const temp = [
+  [5, 8, 12, 10, 7, 9, 11, 14, 16, 13, 10, 6, 4, 3, 2, 4, 6, 8, 10, 12, 15, 17, 18, 16, 13, 10],
+  [1, 4, 6, 7, 9, 11, 13, 15, 12, 9, 7, 5, 3, 2, 3, 6, 8, 10, 12, 15, 17, 19, 18, 16, 13, 11],
+  [8, 10, 13, 15, 16, 18, 19, 20, 17, 15, 13, 11, 10, 9, 8, 10, 12, 14, 16, 18, 20, 22, 21, 18, 16, 14],
+  [2, 5, 7, 9, 12, 15, 17, 18, 15, 13, 11, 8, 6, 5, 4, 7, 9, 12, 14, 16, 19, 21, 20, 18, 16, 13],
+  [6, 8, 11, 14, 16, 18, 20, 21, 18, 15, 12, 10, 8, 6, 5, 8, 10, 13, 15, 18, 20, 22, 21, 19, 16, 13],
+  [11, 14, 17, 19, 21, 23, 24, 22, 19, 16, 13, 11, 10, 9, 9, 12, 15, 18, 20, 23, 25, 27, 26, 24, 21, 18],
+  [9, 11, 14, 16, 18, 20, 22, 21, 18, 16, 13, 11, 9, 8, 7, 10, 13, 16, 18, 21, 23, 24, 23, 21, 18, 15],
+  [7, 10, 13, 15, 17, 20, 22, 23, 20, 17, 14, 12, 10, 9, 8, 11, 14, 17, 19, 22, 24, 26, 25, 23, 20, 17],
+  [3, 6, 9, 11, 13, 15, 17, 18, 16, 14, 11, 9, 7, 6, 5, 8, 10, 13, 15, 17, 19, 21, 20, 18, 15, 12],
+  [1, 3, 5, 7, 9, 11, 13, 15, 12, 9, 7, 5, 3, 2, 3, 6, 8, 10, 12, 15, 17, 19, 18, 16, 13, 11],
+  [6, 8, 11, 14, 16, 18, 20, 21, 18, 15, 12, 10, 8, 6, 5, 8, 10, 13, 15, 18, 20, 22, 21, 19, 16, 13],
+  [10, 13, 16, 18, 20, 22, 23, 24, 21, 18, 15, 13, 11, 10, 9, 12, 15, 18, 20, 23, 25, 27, 26, 24, 21, 18]
+  ];
+  const months = "Jaanuar, Veebruar, Märts, Aprill, Mai, Juuni, Juuli, August, September, Oktoober, November, Detsember";
+  const maksimum = []
+  const minimum = []
+  let sum = 0;
+
+  for (let j = 0; j < temp.length; j++) {
+  maksimum.push(Math.max(...temp[j]));
+  minimum.push(Math.min(...temp[j]));
+  for (let i = 0; i < temp[j].length; i++) {
+    sum += temp[j][i];
+    
+    } 
+    console.log(sum / temp[j].length); 
+    sum = 0
+  }
+
+  
+ console.log(months.split(",")[maksimum.indexOf(Math.max(...maksimum))]);
+ console.log(months.split(",")[minimum.indexOf(Math.min(...minimum))]);
+  //console.log(months.split(",")[5])
+
+
