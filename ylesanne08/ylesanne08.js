e27efd8a4641ac33fcf15f7c58807bd6bd8cb59f
thorian <115464923+styrbbb@users.noreply.github.com>
2024-12-06 09:20:02 +0200

    yl8

--- ylesanne08/ylesanne08.js
+++ ylesanne08/ylesanne08.js
@@ -1,6 +1,14 @@
 //Thorian Perk
-//29.11.2024
+//06.12.2024
 //Javascript ülesanne 8
 
+//mündid
+const myndid = [200, 0.2, 10, 0.01, 2, 1, 0.1, 0.02, 0.05, 100, 5, 0.5, 50, 20];
 
+let i = 0;
+while (i < myndid.sort().length) {
+    console.log(myndid[i]);
+    i++;
+}
+console.log("Kokku on: "+myndid.reduce((a, b) => a + b) +" münti.");
 
