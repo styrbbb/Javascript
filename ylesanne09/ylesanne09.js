1bf34ae808e80ec5a80d7f39c489ee088657bc2a
thorian <115464923+styrbbb@users.noreply.github.com>
2024-12-06 09:20:06 +0200

    yl9

---
+++ ylesanne09/ylesanne09.js
@@ -0,0 +1,12 @@
+//Thorian Perk
+//06.12.2024
+//Javascript ülesanne 9
+
+//Klassika ja noolefunktsioonid
+var erifunkt = function() {
+    console.log("Thorian")
+  };
+  
+  erifunkt();
+
+//Argumentitega funktioonid
