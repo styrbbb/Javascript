83eced432aaed49004d7160fd0a43d788fc533ee
thorian <115464923+styrbbb@users.noreply.github.com>
2024-12-06 09:55:07 +0200

    yl9

--- ylesanne09/ylesanne09.js
+++ ylesanne09/ylesanne09.js
@@ -10,3 +10,35 @@ var erifunkt = function() {
   erifunkt();
 
 //Argumentitega funktioonid
+const event = new Date(Date.UTC(2024, 11, 6, 3, 0, 0));
+const options = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+};
+
+console.log(event.toLocaleDateString('et-EE', options));
+
+//Teadmata hulk
+
+
+//Salajane sõnum
+const salajanesonum = () => {
+    let sonym = "Kas on see salajane?";
+    console.log(sonym.replace(/[aeiouüõäö]/g,"*"));
+  };
+  
+  salajanesonum();
+
+//Unikaalsed nimed
+const nimed = ["Kati", "Mati", "Kati", "Mari", "Mati", "Jüri"];
+
+const unikaalsednimed = () => {
+    unikaalsed = nimed.filter(function(item, pos) {
+        return nimed.indexOf(item) == pos;
+    })
+    console.log(unikaalsed)
+  };
+  
+  unikaalsednimed();
