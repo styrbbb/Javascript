9d4b786588d67938cef16c9fc85e75fc6f661ce3
thorian <115464923+styrbbb@users.noreply.github.com>
2024-12-10 15:32:01 +0200

    yl10

--- ylesanne10/ylesanne10.js
+++ ylesanne10/ylesanne10.js
@@ -1,5 +1,28 @@
 //Thorian Perk
-//06.12.2024
+//10.12.2024
 //Javascript ülesanne 10
 
-//Klassika ja noolefunktsioonid
+//Toote objekt
+const toode = {
+    tooted: [
+        { nimi:'Piim', hind:3.60, kogus:2 },
+        { nimi:'Leib', hind:2.00, kogus:1 },
+        { nimi:'Munad', hind:1.50, kogus:6 },
+        { nimi:'Juust', hind:4.20, kogus:1 },
+        { nimi:'Tomatid', hind:2.30, kogus:3 },
+      ],
+      summa() {
+        let kokku = 0;
+        this.tooted.forEach(toode => kokku += (toode.hind*toode.kogus));  
+        return Math.round(kokku * 100) / 100;
+      } ,
+      lisatoode() {
+        tooted.push({ nimi:'Kartul', hind: 0.80, kogus: 8});
+        console.log(toode);
+      }
+    };
+   
+    console.log(`Ostukorv kokku: ${toode.summa()}`);
+
+
+
