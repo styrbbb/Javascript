b0bf427719ee3b5b407c14ad9f4fd9fafb2d442d
thorian <115464923+styrbbb@users.noreply.github.com>
2024-12-11 10:19:23 +0200

    yl11

--- ylesanne11/ylesanne11.js
+++ ylesanne11/ylesanne11.js
@@ -2,3 +2,11 @@
 //11.12.2024
 //Javascript ülesanne 11
 
+const nimed = ["mari maasikas", "jaan jõesaar", "kristiina kukk", "margus mustikas", "jaak järve", "kadi kask", "Toomas Tamm", "Kadi Meri", "Leena Laas", "Madis Mets", "Hannes Hõbe", "Anu Allikas", "Kristjan Käär", "Eva Esimene", "Jüri Jõgi", "Liis Lepik", "Kalle Kask", "Tiina Teder", "Kaidi Koppel", "tiina Toom"];
+
+const capitalized = nimed.map((eesnimi) => {
+    return eesnimi[0].toUpperCase() + eesnimi.slice(1);
+
+});
+
+console.log(capitalized);
